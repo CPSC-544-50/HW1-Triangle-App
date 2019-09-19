268cb316b7a1c3ffac86999da3fd116d303b8117
krozet <keawa.rozet@yahoo.com>
2019-09-18 18:23:05 -0700

    created a simple input/output system.

--- TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
+++ TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.triangleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button checkNumberButton;
+    EditText numberInputEditText;
+    TextView resultsTextView;
+
+    final static String COMPARE_STRING = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Finds views in the UI and makes them accessible in code
+        resultsTextView = findViewById(R.id.resultsTextView);
+        numberInputEditText = findViewById(R.id.numberInputEditText);
+        setupButton();
+    }
+
+    private void setupButton() {
+        checkNumberButton = findViewById(R.id.checkNumberButton);
+
+        // This handles what happens when the button is clicked
+        checkNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkNumber();
+            }
+        });
+    }
+
+    // Changes resultsTextView based on the user's input
+    private void checkNumber() {
+        if (numberInputEditText.getText().toString().equals(COMPARE_STRING)) {
+            resultsTextView.setText(getResources().getString(R.string.correctString));
+        } else if (numberInputEditText.getText().toString().equals("")) {
+            resultsTextView.setText(getResources().getString(R.string.enterANumberString));
+        } else {
+            resultsTextView.setText(getResources().getString(R.string.incorrectString));
+        }
     }
 }
