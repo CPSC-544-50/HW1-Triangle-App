f497d31a5b3ef8cbf7204d73c796756651e8a05a
Hans <hans.sono23@gmail.com>
2019-09-26 14:58:04 -0500

    Sprint Two Completed
    - Added Input Validation function.
    - Added error messages.
    - Debugged code.

--- TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
+++ TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.triangleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     Button EndButton;
     EditText numberInputEditText;
     TextView resultsTextView;
+    String outputString = "";
+    Float[] floatLengths= new Float[3];
 
     final static String COMPARE_STRING = "1";
 
@@ -32,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         checkNumberButton = findViewById(R.id.checkNumberButton);
         EndButton = findViewById(R.id.EndButton);
         EndButton.setText("Stop");
-        EndButton.setVisibility(View.GONE); //comment out to get stop functionality
-        // This handles what happens when the button is clicked
+//        EndButton.setVisibility(View.GONE);
         checkNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,53 +65,88 @@ public class MainActivity extends AppCompatActivity {
     }
     // Changes resultsTextView based on the user's input
     private String displayText() {
+        outputString = "Good input:\n";
         String[] sideLengths = numberInputEditText.getText().toString().split(",");
-        String outputString = "Good input:\n";
-        if ( sideLengths.length!= 3)
-            outputString = "Not a Comma Seperated List of 3 Numbers";
-        else {
-            int count=0;
-            Float[] floatLengths= new Float[3];
-            for (String side : sideLengths) {
-                try {
-                    floatLengths[count] = Float.parseFloat(side);
-                    outputString += floatLengths[count].toString() ;
-                    if(count++ < sideLengths.length-1)
-                            outputString += " | ";
-                }
-                catch (NumberFormatException | NullPointerException nfe) {
-                    outputString = "Not a Comma Seperated List of 3 Numbers";
-                    break;
-                }
-            }
-//            if(floatLengths[2]!=null)
-//                if(confirmSanity(floatLengths))
-//                    outputString+= "\n" + classifyTriangle(floatLengths);
-//                else
-//                    outputString+= "\n" + "All Triangle side lengths MUST be greater than zero";
+        if (validateInputs(sideLengths)) {
+            outputString+= "\n" + classifyTriangle(floatLengths);
         }
-
         resultsTextView.setText(outputString);
         return outputString;
-
     }
+
+    private boolean validateInputs(String[] sideLengths) {
+        switch(sideLengths.length){
+
+            case 1:
+                try {
+                    Float.parseFloat(sideLengths[0]);
+                }
+                catch (NumberFormatException | NullPointerException nfe) {
+                    outputString = "You need to use commas as delimiters";
+                    return false;
+                }
+                outputString = "You need two more inputs";
+                return false;
+            case 2:
+                try {
+                    Float.parseFloat(sideLengths[0]);
+                    Float.parseFloat(sideLengths[1]);
+                }
+                catch (NumberFormatException | NullPointerException nfe) {
+                    outputString = "Not a Comma Separated List of 3 Numbers";
+                    return false;
+                }
+                outputString = "You need one more input";
+                return false;
+            case 3:
+                int count=0;
+                for (String side : sideLengths) {
+                    try {
+                        floatLengths[count] = Float.parseFloat(side);
+                        outputString += floatLengths[count].toString() ;
+                        if(count++ < sideLengths.length-1)
+                            outputString += " | ";
+                    }
+                    catch (NumberFormatException | NullPointerException nfe) {
+                        outputString = "Not a Comma Separated List of 3 Numbers";
+                        return false;
+                    }
+                }
+
+                if(confirmSanity(floatLengths)) {
+                    return true;
+                } else {
+                    outputString+= "\n" + "All Triangle side lengths MUST be greater than zero";
+                    return false;
+                }
+
+            default:
+                outputString = "You have more than 3 inputs";
+                return false;
+        }
+    }
+
     private boolean confirmTriangularity(Float sides[]){
         return sides[0]<sides[1]+sides[2] && sides[1]<sides[0]+sides[2] && sides[2]<sides[0]+sides[1];
     }
+
     private boolean confirmSanity(Float sides[]){
         return sides[0]>0 && sides[1]>0 && sides[2]>0;
     }
+
     private String classifyTriangle(Float sides[]) {
         if(!confirmTriangularity(sides))
-            return "-- This is not a valid triangle. One side is longer that the sum of the other two sides --";
-        if(sides[0]==sides[1])
-            if(sides[1]==sides[2])
-                return "--Equalateral--";
+            return "-- This is not a valid triangle. One side is longer (or equal) that the sum of the other two sides --";
+        if(Float.compare(sides[0],sides[1]) == 0) {
+            if (Float.compare(sides[1],sides[2]) == 0)
+                return "--Equilateral--";
             else
-                return "--Equalateral--";
-        else if(sides[1]==sides[2])
-            return "--Iscoceles--";
-        else
+                return "--Isosceles--";
+        } else if(Float.compare(sides[1],sides[2]) == 0) {
+            return "--Isosceles--";
+        }
+        else {
             return "--Scalene--";
+        }
     }
 }
