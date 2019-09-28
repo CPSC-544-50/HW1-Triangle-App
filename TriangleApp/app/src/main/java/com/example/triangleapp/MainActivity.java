0373605accefe464a4d80f0d72a29c10752c3bde
krozet <keawa.rozet@yahoo.com>
2019-09-28 15:57:59 -0700

    added more indepth error checking, checks each argument. also made format closer to the format the professor needs.

--- TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
+++ TriangleApp/app/src/main/java/com/example/triangleapp/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     EditText numberInputEditText;
     TextView resultsTextView;
     String outputString = "";
+    String input = "";
     Float[] floatLengths= new Float[3];
 
     private List<String> inputHistory = new ArrayList<String>();
@@ -124,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Changes resultsTextView based on the user's input
     private void displayText() {
-        String input= numberInputEditText.getText().toString();
+        input = numberInputEditText.getText().toString();
         inputHistory.add(input);
         inputPointer=inputHistory.size();
         String[] sideLengths = input.split(",");
@@ -142,66 +143,49 @@ public class MainActivity extends AppCompatActivity {
     private boolean validateInputs(String[] sideLengths) {
         boolean valid = true;
         switch(sideLengths.length){
+            case 0:
+                outputString = formattedInputValues(true) + formattedInputValues(false);
+                outputString += "Please enter a comma-separated list of three numbers. Use the format xx,xx,xx\n\n";
+                valid = false;
+                break;
             case 1:
-                try {
-                    Float.parseFloat(sideLengths[0]);
+                outputString = formattedInputValues(true) + formattedInputValues(false);
+                outputString += checkForValidEntry(sideLengths[0], 0);
 
-                }
-                catch (NumberFormatException | NullPointerException nfe) {
-                    outputString += "You need to use commas to separate the three values\n\n";
-                }
                 outputString += "You need two more values\n\n";
+                outputString += "You need to use commas to separate the three values\n\n";
                 valid = false;
                 break;
             case 2:
-                try {
-                    Float.parseFloat(sideLengths[0]);
-                    Float.parseFloat(sideLengths[1]);
-                }
-                catch (NumberFormatException | NullPointerException nfe) {
-                    outputString += "Not a comma-separated list of three numbers. Please use the format xx,xx,xx\n\n";
-                }
+                outputString = formattedInputValues(true) + formattedInputValues(false);
+                outputString += checkForValidEntry(sideLengths[0], 0);
+                outputString += checkForValidEntry(sideLengths[1], 1);
+
                 outputString += "You need one more value\n\n";
+                outputString += "Not a comma-separated list of three numbers. Please use the format xx,xx,xx\n\n";
                 valid = false;
                 break;
             case 3:
-                int count=0;
-                String formattedUserInput = "[";
-                for (String side : sideLengths) {
-                    try {
-                        floatLengths[count] = Float.parseFloat(side);
-                        formattedUserInput += floatLengths[count].toString() ;
-                        if(count++ < sideLengths.length-1)
-                            formattedUserInput += ", ";
-                    }
-                    catch (NumberFormatException | NullPointerException nfe) {
-                        outputString = "Not a comma-separated list of three numbers. Please use the format xx,xx,xx\n\n";
-                        return false;
-                    }
-                }
-                formattedUserInput += "] = ";
+                outputString = formattedInputValues(true) + formattedInputValues(false);
+                String errorMessage = "";
 
-                if(!confirmSanity(floatLengths)) {
-                    outputString += "All Triangle side lengths must be greater than zero\n\n";
-                    valid = false;
+                errorMessage += checkForValidEntry(sideLengths[0], 0);
+                errorMessage += checkForValidEntry(sideLengths[1], 1);
+                errorMessage += checkForValidEntry(sideLengths[2], 2);
+
+                if(errorMessage.length() > 0) {
+                    outputString += errorMessage + "Not a comma-separated list of three numbers. Please use the format xx,xx,xx\n\n";
+                    return false;
                 }
 
-                if(!confirmUpperBound(floatLengths)) {
-                    outputString += "All Triangle side lengths cannot be greater than 100\n\n";
-                    valid = false;
-                }
-
+                setFloatLengths(sideLengths);
                 if(!confirmTriangularity(floatLengths)) {
                     outputString += "This is not a valid triangle. One side is longer (or equal) that the sum of the other two sides\n\n";
                     valid = false;
                 }
-
-                if(valid) {
-                    outputString = formattedUserInput;
-                }
-
                 break;
             default:
+                outputString = formattedInputValues(true) + formattedInputValues(false);
                 outputString += "You have more than three values entered\n\n";
                 valid = false;
                 break;
@@ -209,16 +193,59 @@ public class MainActivity extends AppCompatActivity {
         return valid;
     }
 
+    private String formattedInputValues(boolean showInput) {
+        if (showInput) {
+            return "Input: Values for TriangleApp?: " + input + "\n";
+        } else {
+            // show output
+            return "Output: [" + input + "] = ";
+        }
+    }
+
+    private void setFloatLengths(String[] sideLengths) {
+        for(int i=0; i<sideLengths.length; i++) {
+            floatLengths[i] = Float.parseFloat(sideLengths[i]);
+        }
+    }
+
+    private String checkForValidEntry(String entry, int index) {
+        String errorMessage = "";
+        Float number;
+
+        String entryIndex = "";
+        switch (index) {
+            case 0:
+                entryIndex = "The first value ";
+                break;
+            case 1:
+                entryIndex = "The second value ";
+                break;
+            case 2:
+                entryIndex = "The third value ";
+                break;
+        }
+
+        //check if the value is a number
+        try {
+            number = Float.parseFloat(entry);
+        }
+        catch (NumberFormatException | NullPointerException nfe) {
+            errorMessage += entryIndex + "needs to be a number\n\n";
+            return errorMessage;
+        }
+
+        if (number < 1 && number >= 0) {
+            errorMessage += entryIndex + "needs to be greater than or equal to 1\n\n";
+        } else if (number < 0) {
+            errorMessage += entryIndex + "needs to be a positive number\n\n";
+        } else if (number > 100) {
+            errorMessage += entryIndex + "needs to be less than or equal to 100\n\n";
+        }
+        return errorMessage;
+    }
+
     private boolean confirmTriangularity(Float sides[]){
         return sides[0]<sides[1]+sides[2] && sides[1]<sides[0]+sides[2] && sides[2]<sides[0]+sides[1];
-    }
-
-    private boolean confirmSanity(Float sides[]){
-        return sides[0]>0 && sides[1]>0 && sides[2]>0;
-    }
-
-    private boolean confirmUpperBound(Float sides[]){
-        return sides[0]<=100 && sides[1]<=100 && sides[2]<=100;
     }
 
     private String classifyTriangle(Float sides[]) {
